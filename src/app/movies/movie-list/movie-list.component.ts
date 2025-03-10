307b4e44569d43bde85f68785c7088455debea6c
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-09 20:16:58 -0600

    Adding new styles and icon to remove movie button

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,11 +1,13 @@
 import { CurrencyPipe, DatePipe, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { GenericListComponent } from "../../shared/components/generic-list/generic-list.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage, GenericListComponent],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage, GenericListComponent, MatButtonModule, MatIconModule],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
