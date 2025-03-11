6f71af070275ffcd21daa3bc3c1757e183f06760
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-10 19:08:12 -0600

    Adding actors submenu

---
+++ src/app/actors/create-actors/create-actors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-actors',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-actors.component.html',
+  styleUrl: './create-actors.component.css'
+})
+export class CreateActorsComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
+import { IndexActorsComponent } from './actors/index-actors/index-actors.component';
+import { CreateActorsComponent } from './actors/create-actors/create-actors.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'generos', component: IndexGenresComponent },
   { path: 'generos/crear', component: CreateGenreComponent },
+  { path: 'actores', component: IndexActorsComponent },
+  { path: 'actores/crear', component: CreateActorsComponent },
 ];
