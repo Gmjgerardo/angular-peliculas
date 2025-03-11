2df1521bd81abc1da4cfe993a3eac4d999bab5c1
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-10 20:39:03 -0600

    Adding edit components and routes in all parts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,22 @@ import { CreateActorsComponent } from './actors/create-actors/create-actors.comp
 import { IndexCinemasComponent } from './cinemas/index-cinemas/index-cinemas.component';
 import { CreateCinemaComponent } from './cinemas/create-cinema/create-cinema.component';
 import { CreateMoviesComponent } from './movies/create-movies/create-movies.component';
+import { EditGenreComponent } from './genres/edit-genre/edit-genre.component';
+import { EditActorComponent } from './actors/edit-actor/edit-actor.component';
+import { EditCinemaComponent } from './cinemas/edit-cinema/edit-cinema.component';
+import { EditMovieComponent } from './movies/edit-movie/edit-movie.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'peliculas/crear', component: CreateMoviesComponent },
+  { path: 'peliculas/editar/:id', component: EditMovieComponent },
   { path: 'generos', component: IndexGenresComponent },
   { path: 'generos/crear', component: CreateGenreComponent },
+  { path: 'generos/editar/:id', component: EditGenreComponent },
   { path: 'actores', component: IndexActorsComponent },
   { path: 'actores/crear', component: CreateActorsComponent },
+  { path: 'actores/editar/:id', component: EditActorComponent },
   { path: 'cines', component: IndexCinemasComponent },
   { path: 'cines/crear', component: CreateCinemaComponent },
+  { path: 'cines/editar/:id', component: EditCinemaComponent },
 ];
