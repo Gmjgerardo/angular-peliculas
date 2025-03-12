4c66df045d6e5df6732c55dcfc9a803a56be7960
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-11 20:24:41 -0600

    First form input addings

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,18 +1,38 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-genre',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, ReactiveFormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
-  router: Router = inject(Router);
+  private router: Router = inject(Router);
+  private formBuilder: FormBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    name: ['', {validators: [Validators.required]}]
+  });
+
+  obtainErrorNameField(): string {
+    const { name } = this.form.controls;
+    let error: string = "";
+
+    if (name.hasError('required')) {
+      error = "El campo nombre es requerido";
+    }
+
+    return error;
+  }
 
   saveChanges(): void {
-    this.router.navigate(['/generos']);
+    // this.router.navigate(['/generos']);
+    console.log(this.form.value);
   }
 }
