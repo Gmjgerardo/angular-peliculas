e9b8906d023007dc34a073e76e335d3325ed2907
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-13 18:32:27 -0600

    Change actor index (basic view)

--- src/app/actors/index-actors/index-actors.component.ts
+++ src/app/actors/index-actors/index-actors.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index-actors',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './index-actors.component.html',
   styleUrl: './index-actors.component.css'
 })
