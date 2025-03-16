bfaf1483ac62b2b0f491afd5bbe6684e92876636
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-16 14:38:22 -0600

    Adding cinema form to create and edit components

--- src/app/cinemas/create-cinema/create-cinema.component.ts
+++ src/app/cinemas/create-cinema/create-cinema.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CinemaFormComponent } from "../cinema-form/cinema-form.component";
+import { CinemaCreateDTO } from '../cinemas';
 
 @Component({
   selector: 'app-create-cinema',
   standalone: true,
-  imports: [],
+  imports: [CinemaFormComponent],
   templateUrl: './create-cinema.component.html',
   styleUrl: './create-cinema.component.css'
 })
 export class CreateCinemaComponent {
-
+  saveChanges(cinema: CinemaCreateDTO): void {
+    console.log('Creando:', cinema);
+  }
 }
--- src/app/cinemas/edit-cinema/edit-cinema.component.ts
+++ src/app/cinemas/edit-cinema/edit-cinema.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CinemaFormComponent } from "../cinema-form/cinema-form.component";
+import { CinemaCreateDTO, CinemaDTO } from '../cinemas';
 
 @Component({
   selector: 'app-edit-cinema',
   standalone: true,
-  imports: [],
+  imports: [CinemaFormComponent],
   templateUrl: './edit-cinema.component.html',
   styleUrl: './edit-cinema.component.css'
 })
 export class EditCinemaComponent {
   @Input({ transform: numberAttribute })
   id!: number;
+
+  // Static value for testing
+  cinema: CinemaDTO = { id: 23, name: 'FORUM Tlaquepaque' }
+
+  saveChanges(cinema: CinemaCreateDTO): void {
+    console.log('Editando:', cinema);
+    
+  }
 }
