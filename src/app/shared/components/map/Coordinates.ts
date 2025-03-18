01a0126b74a30b00dbf4d899eac08a1571409a41
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-17 21:57:55 -0600

    Creating map component

---
+++ src/app/shared/components/map/Coordinates.ts
@@ -0,0 +1,5 @@
+export interface Coordinate {
+  text?: string;
+  lat: number;
+  lng: number;
+}
---
+++ src/app/shared/components/map/map.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { icon, latLng, LeafletMouseEvent, MapOptions, marker, Marker, MarkerOptions, tileLayer } from 'leaflet';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { Coordinate } from './Coordinates';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [LeafletModule],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit {
+  @Output() selectedCoordinate: EventEmitter<Coordinate> = new EventEmitter<Coordinate>();
+  
+  @Input() initialCoordinates: Coordinate[] = [];
+  @Input() height: number = 200;
+  @Input() width: number = 200;
+
+  markers: Marker<any>[] = [];
+
+  options: MapOptions = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+      })
+    ],
+    zoom: 14,
+    center: latLng(20.648204004447127, -103.31999658139843)
+  }
+
+  private markerOptions: MarkerOptions = {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/marker-icon.png',
+      iconRetinaUrl: 'assets/marker-icon-2x.png',
+      shadowUrl: 'assets/marker-shadow.png'
+    })
+  }
+
+  ngOnInit(): void {
+    // Convert coordinate points into markers to show in the map
+    this.markers = this.initialCoordinates.map(point => marker([point.lat, point.lng], this.markerOptions)) as unknown as Marker<any>[];
+  }
+
+  clickHandle(event: LeafletMouseEvent): void {
+    const {lat, lng } = event.latlng;
+
+    this.markers = [];
+    this.markers.push(marker([lat, lng], this.markerOptions))
+    this.selectedCoordinate.emit({lat, lng} as Coordinate);
+  }
+}
