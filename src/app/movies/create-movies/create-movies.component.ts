375ed58fb0541a2bb79cf39e4a3eb101f0c91fbc
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-18 18:43:20 -0600

    Adding movie form to create and edit component

--- src/app/movies/create-movies/create-movies.component.ts
+++ src/app/movies/create-movies/create-movies.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MovieFormComponent } from "../movie-form/movie-form.component";
+import { MovieCreateDTO } from '../movies';
 
 @Component({
   selector: 'app-create-movies',
   standalone: true,
-  imports: [],
+  imports: [MovieFormComponent],
   templateUrl: './create-movies.component.html',
   styleUrl: './create-movies.component.css'
 })
 export class CreateMoviesComponent {
-
+  saveChanges(movie: MovieCreateDTO): void {
+    console.log('Creando:', movie);
+  }
 }
--- src/app/movies/edit-movie/edit-movie.component.ts
+++ src/app/movies/edit-movie/edit-movie.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { MovieFormComponent } from "../movie-form/movie-form.component";
+import { MovieCreateDTO, MovieDTO } from '../movies';
 
 @Component({
   selector: 'app-edit-movie',
   standalone: true,
-  imports: [],
+  imports: [MovieFormComponent],
   templateUrl: './edit-movie.component.html',
   styleUrl: './edit-movie.component.css'
 })
 export class EditMovieComponent {
   @Input({ transform: numberAttribute })
   id!: number;
+
+  // Static value for testing
+  movie: MovieDTO = { id: 1, title: 'Star Wars: Episode III, Revenge Of The Sith', releaseDate: new Date('2005-05-15'), image: 'https://upload.wikimedia.org/wikipedia/en/9/93/Star_Wars_Episode_III_Revenge_of_the_Sith_poster.jpg?20171214071623', trailer: 'ABC' };
+
+  saveChanges(movie: MovieCreateDTO): void {
+    console.log('Editando:', movie);
+  }
 }
