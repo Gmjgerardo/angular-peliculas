98faa17628f5f4092a4fe0011f5b5981d009fa42
Gerardo <106112451+Gmjgerardo@users.noreply.github.com>
2025-03-20 20:33:14 -0600

    Creating multiple selector component for using in forms

---
+++ src/app/shared/components/multiple-selector/multiple-selector.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { MultipleSelectorDTO } from './MultipleSelectorModel';
+
+@Component({
+  selector: 'app-multiple-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiple-selector.component.html',
+  styleUrl: './multiple-selector.component.css'
+})
+export class MultipleSelectorComponent {
+  @Input({ required: true, }) selected!: MultipleSelectorDTO[];
+
+  @Input({ required: true, }) notSelected!: MultipleSelectorDTO[];
+
+  select(element: MultipleSelectorDTO, index: number): void {
+    this.selected.push(element);
+    this.notSelected.splice(index, 1);
+  }
+
+  unselect(element: MultipleSelectorDTO, index: number): void {
+    this.notSelected.push(element);
+    this.selected.splice(index, 1);
+  }
+
+  selectAll(): void {
+    this.selected.push(...this.notSelected);
+    this.notSelected = [];
+  }
+
+  unselectAll(): void {
+    this.notSelected.push(...this.selected);
+    this.selected = [];
+  }
+}
