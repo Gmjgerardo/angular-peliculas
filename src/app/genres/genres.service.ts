d88a58e827a6337490ba7911ff8a0f9e0abe1bde
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-06 15:06:30 -0600

    Adding differents environments to access API's

--- src/app/genres/genres.service.ts
+++ src/app/genres/genres.service.ts
@@ -2,16 +2,18 @@ import { inject, Injectable } from '@angular/core';
 import { GenreDTO } from './genres';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenresService {
   private http: HttpClient = inject(HttpClient);
+  private baseURL:string = `${environment.apiURL}/genres`;
 
   constructor() { }
 
   public obtainAll(): Observable<GenreDTO[]> {
-    return this.http.get<GenreDTO[]>("https://localhost:7165/api/Genres");
+    return this.http.get<GenreDTO[]>(this.baseURL);
   }
 }
