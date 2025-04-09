8f766994e21a2d8274a7590561adaad55a940b1b
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-09 15:06:02 -0600

    Adding error-list component to create genres for server errors managment

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -7,11 +7,14 @@ import { Router } from '@angular/router';
 import { GenreFormComponent } from "../genre-form/genre-form.component";
 import { GenreCreationDTO } from '../genres';
 import { GenresService } from '../genres.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { extractErrors } from '../../shared/functions/index';
+import { ErrorListComponent } from "../../shared/components/error-list/error-list.component";
 
 @Component({
   selector: 'app-create-genre',
   standalone: true,
-  imports: [MatButtonModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, GenreFormComponent],
+  imports: [MatButtonModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, GenreFormComponent, ErrorListComponent],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
@@ -19,7 +22,14 @@ export class CreateGenreComponent {
   private router: Router = inject(Router);
   private genreService: GenresService = inject(GenresService);
 
+  errors: string[] = [];
+
   saveChanges(genre: GenreCreationDTO): void {
-    this.genreService.create(genre).subscribe(() => this.router.navigate(['/generos']));
+    this.genreService.create(genre).subscribe({
+      next: () => this.router.navigate(['/generos']),
+      error: (err: HttpErrorResponse) => {
+        this.errors = extractErrors(err);
+      }
+    });
   }
 }
