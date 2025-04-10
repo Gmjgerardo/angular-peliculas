8858d1c46297db2c57905c2373537f007486dedb
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-09 21:52:33 -0600

    Showing genres in a table (index)

--- src/app/genres/index-genres/index-genres.component.ts
+++ src/app/genres/index-genres/index-genres.component.ts
@@ -3,20 +3,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { GenresService } from '../genres.service';
 import { GenreDTO } from '../genres';
+import { GenericListComponent } from "../../shared/components/generic-list/generic-list.component";
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-index-genres',
   standalone: true,
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, GenericListComponent, MatTableModule, MatButtonModule, RouterLink],
   templateUrl: './index-genres.component.html',
   styleUrl: './index-genres.component.css'
 })
 export class IndexGenresComponent {
   genresService: GenresService = inject(GenresService);
+  genres!: GenreDTO[];
+  displayedColumns: string[] = ['id', 'name', 'actions'];
 
   constructor() {
     this.genresService.obtainAll().subscribe(genres => {
-      console.log(genres);
+      this.genres = genres;
     });
   }
 }
