43d33672594853fa859dabe9636f2f607824dd6b
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-17 01:19:02 -0600

    Implementing general entity index in actor and genres indexes

--- src/app/actors/index-actors/index-actors.component.ts
+++ src/app/actors/index-actors/index-actors.component.ts
@@ -1,57 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
 import { ActorsService } from '../actors.service';
-import { ActorDTO } from '../actors';
-import { HttpResponse } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';
-import { GenericListComponent } from "../../shared/components/generic-list/generic-list.component";
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { PaginationDTO } from '../../shared/models/PaginationDTO';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { EntityIndexComponent } from "../../shared/components/entity-index/entity-index.component";
+import { CRUD_SERVICE_TOKEN } from '../../shared/providers/providers';
 
 @Component({
   selector: 'app-index-actors',
   standalone: true,
-  imports: [MatButtonModule, RouterLink, MatTableModule, MatButtonModule, GenericListComponent, MatPaginatorModule, SweetAlert2Module],
+  imports: [EntityIndexComponent],
   templateUrl: './index-actors.component.html',
-  styleUrl: './index-actors.component.css'
+  styleUrl: './index-actors.component.css',
+  providers: [{ provide: CRUD_SERVICE_TOKEN, useClass: ActorsService }],
 })
-export class IndexActorsComponent implements OnInit {
-  private actorsService: ActorsService = inject(ActorsService);
+export class IndexActorsComponent {
 
-  actors: ActorDTO[] = [];
-  columnsToDisplay: string[] = ['id', 'name', 'actions'];
-  pagination: PaginationDTO = { page: 1, rowsPerPage: 5 };
-  totalRecords!: number;
-  swalConfigs: Object = {
-    titleText: 'Confirmación',
-    text: '¿Deseas borrar este registro?',
-    showCancelButton: true,
-    cancelButtonText: 'Cancelar',
-  };
-
-  private getRows(): void {
-    this.actorsService.obtainAll(this.pagination).subscribe((response: HttpResponse<ActorDTO[]>) => {
-      const { headers, body } = response;
-      this.actors = body as ActorDTO[];
-      this.totalRecords = Number(headers.get('total-records-count'));
-    });
-  }
-  ngOnInit(): void {
-    this.getRows();
-  }
-
-  updateTable(pagination: PageEvent) {
-    const { pageIndex: page, pageSize: rowsPerPage } = pagination;
-    this.pagination = {page: page + 1, rowsPerPage};
-    this.getRows();
-    }
-
-  deleteRow(id: number): void {
-    this.actorsService.delete(id).subscribe(() => {
-      this.pagination = { page: 1, rowsPerPage: 5 };
-      this.getRows();
-    });
-  }
 }
--- src/app/genres/index-genres/index-genres.component.ts
+++ src/app/genres/index-genres/index-genres.component.ts
@@ -1,59 +1,16 @@
-import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
 import { GenresService } from '../genres.service';
-import { GenreDTO } from '../genres';
-import { GenericListComponent } from "../../shared/components/generic-list/generic-list.component";
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { HttpResponse } from '@angular/common/http';
-import { PaginationDTO } from '../../shared/models/PaginationDTO';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { EntityIndexComponent } from "../../shared/components/entity-index/entity-index.component";
+import { CRUD_SERVICE_TOKEN } from '../../shared/providers/providers';
 
 @Component({
   selector: 'app-index-genres',
   standalone: true,
-  imports: [MatButtonModule, RouterLink, GenericListComponent, MatTableModule, MatButtonModule, RouterLink, MatPaginatorModule, SweetAlert2Module],
+  imports: [EntityIndexComponent],
   templateUrl: './index-genres.component.html',
-  styleUrl: './index-genres.component.css'
+  styleUrl: './index-genres.component.css',
+  providers: [{ provide: CRUD_SERVICE_TOKEN, useClass: GenresService }],
 })
 export class IndexGenresComponent {
-  private genresService: GenresService = inject(GenresService);
 
-  genres!: GenreDTO[];
-  displayedColumns: string[] = ['id', 'name', 'actions'];
-  pagination: PaginationDTO = { page: 1, rowsPerPage: 5 };
-  rowsCount!: number;
-  swalConfigs: Object = {
-    title: 'Confirmación',
-    text: '¿Deseas borrar este registro?',
-    showCancelButton: true,
-    cancelButtonText: 'Cancelar',
-    };
-
-  constructor() {
-    this.getRows();
-  }
-
-  getRows(): void {
-    this.genresService.obtainAll(this.pagination)
-    .subscribe((response: HttpResponse<GenreDTO[]>) => {
-      const {headers, body} = response;
-
-      this.genres = body as GenreDTO[];
-      this.rowsCount = Number(headers.get('total-records-count'));
-    });
-  }
-
-  updateTable(pag: PageEvent): void {
-    this.pagination = { page: pag.pageIndex + 1, rowsPerPage: pag.pageSize };
-    this.getRows();
-  }
-
-  deleteRow(id: number): void {
-    this.genresService.delete(id).subscribe(() => {
-      this.pagination = { page: 1, rowsPerPage: 5 };
-      this.getRows();
-    });
-  }
 }
