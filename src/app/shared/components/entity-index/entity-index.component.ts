0a2c1465e366f683631f22da339201132014f71b
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-04-17 01:10:20 -0600

    Creating/Separating entity index for reutilize code

---
+++ src/app/shared/components/entity-index/entity-index.component.ts
@@ -0,0 +1,64 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { PaginationDTO } from '../../models/PaginationDTO';
+import { HttpResponse } from '@angular/common/http';
+import { GenericListComponent } from "../generic-list/generic-list.component";
+import { RouterLink } from '@angular/router';
+import { capitalizeString } from '../../../shared/functions';
+import { CRUD_SERVICE_TOKEN } from '../../providers/providers';
+
+@Component({
+  selector: 'app-entity-index',
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatButtonModule, SweetAlert2Module, GenericListComponent, RouterLink,],
+  templateUrl: './entity-index.component.html',
+  styleUrl: './entity-index.component.css'
+})
+export class EntityIndexComponent<TDTO> implements OnInit {
+  private CRUDService = inject(CRUD_SERVICE_TOKEN) as any;
+
+  @Input({ required: true }) title: string = '';
+  @Input({ required: true }) editURL: string = '';
+  @Input() columnsToDisplay: string[] = ['id', 'name', 'actions'];
+
+  entities!: TDTO[];
+  totalRecords!: number;
+
+  pagination: PaginationDTO = { page: 1, rowsPerPage: 5 };
+  swalConfigs: Object = {
+    titleText: 'Confirmación',
+    text: '¿Deseas borrar este registro?',
+    showCancelButton: true,
+    cancelButtonText: 'Cancelar',
+  };
+
+  private getRows(): void {
+    this.CRUDService.obtainAll(this.pagination).subscribe((response: HttpResponse<TDTO[]>) => {
+      const { headers, body } = response;
+      this.entities = body as TDTO[];
+      this.totalRecords = Number(headers.get('total-records-count'));
+    });
+  }
+
+  capitalize = (txt: string): string => capitalizeString(txt);
+
+  ngOnInit(): void {
+    this.getRows();
+  }
+
+  updateTable(pagination: PageEvent) {
+    const { pageIndex: page, pageSize: rowsPerPage } = pagination;
+    this.pagination = {page: page + 1, rowsPerPage};
+    this.getRows();
+  }
+
+  deleteRow(id: number): void {
+    this.CRUDService.delete(id).subscribe(() => {
+      this.pagination.page = 1;
+      this.getRows();
+    });
+  }
+}
