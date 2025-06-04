28a764c14e078462654cdbdd710e56315d87bc8e
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-06-04 15:35:49 -0600

    Modifying/Updating Create Movie form and functionality

--- src/app/movies/create-movies/create-movies.component.ts
+++ src/app/movies/create-movies/create-movies.component.ts
@@ -1,29 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MovieFormComponent } from "../movie-form/movie-form.component";
 import { MovieCreateDTO } from '../movies';
 import { MultipleSelectorDTO } from '../../shared/components/multiple-selector/MultipleSelectorModel';
+import { MoviesService } from '../movies.service';
+import { Router } from '@angular/router';
+import { extractErrors } from '../../shared/functions';
+import { ErrorListComponent } from "../../shared/components/error-list/error-list.component";
+import { LoadingComponent } from "../../shared/components/loading/loading.component";
 
 @Component({
   selector: 'app-create-movies',
   standalone: true,
-  imports: [MovieFormComponent],
+  imports: [MovieFormComponent, ErrorListComponent, LoadingComponent, ErrorListComponent],
   templateUrl: './create-movies.component.html',
   styleUrl: './create-movies.component.css',
 })
 export class CreateMoviesComponent {
-  notSelectedGenres: MultipleSelectorDTO[] = [
-    { key: 1, value: 'Drama' },
-    { key: 2, value: 'Acción' },
-    { key: 3, value: 'Comedia' },
-  ];
+  private moviesService: MoviesService = inject(MoviesService);
+  private router: Router = inject(Router);
 
-  notSelectedCinemas: MultipleSelectorDTO[] = [
-    { key: 1, value: 'Cinepolis: FORUM Tlaquepaque' },
-    { key: 2, value: 'Cinepolis: La Perla' },
-    { key: 3, value: 'Cinemex: Tonalá' },
-  ];
+  selectedGenres: MultipleSelectorDTO[] = [];
+  notSelectedGenres: MultipleSelectorDTO[] = [];
+  selectedCinemas: MultipleSelectorDTO[] = [];
+  notSelectedCinemas: MultipleSelectorDTO[] = [];
+  selectedActors: MultipleSelectorDTO[] = [];
+  errors: string[] = [];
+
+  constructor() {
+    this.moviesService.createGet().subscribe(model => {
+      // Adding genres to selector
+      this.notSelectedGenres = model.genres.map(genre => {
+        return <MultipleSelectorDTO>{ key: genre.id, value: genre.name };
+      });
+
+      // Adding cinemas to selector
+      this.notSelectedCinemas = model.cinemas.map(cinema => {
+        return <MultipleSelectorDTO>{ key: cinema.id, value: cinema.name };
+      });
+    });
+  }
 
   saveChanges(movie: MovieCreateDTO): void {
-    console.log('Creando:', movie);
+    this.moviesService.create(movie).subscribe({
+      next: movie => {
+        console.log(movie);
+        this.router.navigate(['/']);
+      },
+      error: err => this.errors = extractErrors(err),
+    });
   }
 }
