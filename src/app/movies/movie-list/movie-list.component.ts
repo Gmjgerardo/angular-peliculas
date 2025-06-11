5ba4fdec30cd48a778f493f084bc33201ef647fe
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-06-11 13:53:15 -0600

    Adding redirect url in edit movie buttons (landing page)

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -3,11 +3,12 @@ import { Component, Input } from '@angular/core';
 import { GenericListComponent } from "../../shared/components/generic-list/generic-list.component";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [NgOptimizedImage, GenericListComponent, MatButtonModule, MatIconModule],
+  imports: [NgOptimizedImage, GenericListComponent, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
