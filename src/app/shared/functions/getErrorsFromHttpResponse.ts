0a1b559692c0933c66a0aea49c8c13df2d96bda1
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-06-19 19:40:57 -0600

    Adding BadRequest (ActionResult in dotnet) extract errors

--- src/app/shared/functions/getErrorsFromHttpResponse.ts
+++ src/app/shared/functions/getErrorsFromHttpResponse.ts
@@ -4,13 +4,23 @@ export function extractErrors(errorResponse: HttpErrorResponse): string[] {
   const { errors } = errorResponse.error;
   const errorList: string[] = [];
 
-  for (const errType in errors) {
-    // Extract each errorArray for all types/values/fields that have at least one error
-    let errorsFromType: string[] = errors[errType];
+  if (errors) {
+    for (const errType in errors) {
+      // Extract each errorArray for all types/values/fields that have at least one error
+      let errorsFromType: string[] = errors[errType];
+  
+      // Mergin all errors with a new pattern "ErrorType: ErrorMessage"
+      errorsFromType = errorsFromType.map(error => `${errType}: ${error}`);
+      errorList.push(...errorsFromType);
+    }
+  }
 
-    // Mergin all errors with a new pattern "ErrorType: ErrorMessage"
-    errorsFromType = errorsFromType.map(error => `${errType}: ${error}`);
-    errorList.push(...errorsFromType);
+  if (Array.isArray(errorResponse.error)) {
+    for (const errElement of errorResponse.error) {
+      const { code, description } = errElement;
+
+      errorList.push(code ? `${code}: ${description}` : description);
+    }
   }
 
   return errorList;
