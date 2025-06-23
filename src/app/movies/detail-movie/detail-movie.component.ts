f2d7336cce92d0cc963cb7cc56e97260d24631fe
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-06-23 13:07:17 -0600

    Implementing rating component to movie details view/component

--- src/app/movies/detail-movie/detail-movie.component.ts
+++ src/app/movies/detail-movie/detail-movie.component.ts
@@ -7,16 +7,22 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Coordinate } from '../../shared/components/map/Coordinates';
 import { MapComponent } from "../../shared/components/map/map.component";
 import { RouterLink } from '@angular/router';
+import { RatingComponent } from "../../shared/components/rating/rating.component";
+import { RatingsService } from '../../ratings/ratings.service';
+import { SecurityService } from '../../security/security.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detail-movie',
   standalone: true,
-  imports: [LoadingComponent, MatChipsModule, MapComponent, RouterLink],
+  imports: [LoadingComponent, MatChipsModule, MapComponent, RouterLink, RatingComponent],
   templateUrl: './detail-movie.component.html',
   styleUrl: './detail-movie.component.css'
 })
 export class DetailMovieComponent implements OnInit {
   private movieService: MoviesService = inject(MoviesService);
+  private ratingsService: RatingsService = inject(RatingsService);
+  private securityService: SecurityService = inject(SecurityService);
   private sanitizer: DomSanitizer = inject(DomSanitizer);
 
   @Input({ required: true, transform: numberAttribute }) id!: number;
@@ -62,5 +68,20 @@ export class DetailMovieComponent implements OnInit {
     }
 
     return result;
+  }
+
+  rate(value: number): void {
+    if (this.securityService.isLogged())
+      this.ratingsService
+        .rateMovie({ movieId: this.movie.id, rate: value })
+        .subscribe({
+          next: () => Swal.fire('Exitoso', "Su voto ha sido recibido", 'success'),
+          error: (err) => {
+            console.error(err);
+            Swal.fire('Error', "Ocurrio un error al intentar registrar el voto", 'error');
+          },
+        });
+    else 
+        Swal.fire('Error', "Debes estar logueado para poder votar por una película", 'error');
   }
 }
